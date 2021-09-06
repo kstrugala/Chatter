e7e0d049f910028a5ce6f8fd9a5575d3ad84666e
Krzysztof Strugała <kr.strugala@gmail.com>
2021-09-06 21:12:59 +0200

    Add unit tests project and add User entity tests

---
+++ tests/Chatter.Tests/Entities/UserTests.cs
@@ -0,0 +1,59 @@
+﻿using Chatter.Core.Entities;
+using Chatter.Core.Exceptions;
+using Microsoft.AspNetCore.Identity;
+using Xunit;
+
+namespace Chatter.Tests.Entities
+{
+    public class UserTests
+    {
+        private readonly User _user;
+        private readonly PasswordHasher<User> _passwordHasher;
+
+        public UserTests()
+        {
+            // Arrange
+            _user = new User("test@test.com", Role.User);
+            _passwordHasher = new PasswordHasher<User>();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void When_first_name_is_null_or_whitespace_SetFirstName_method_should_throw_DomainException(string firstName)
+        {
+            Assert.Throws<DomainException>(() =>
+            {
+                // Act & Assert
+                _user.SetFirstName(firstName);
+            });
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void When_last_name_is_null_or_whitespace_SetLastName_method_should_throw_DomainException(string lastName)
+        {
+            Assert.Throws<DomainException>(() =>
+            {
+                // Act & Assert
+                _user.SetLastName(lastName);
+            });
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void When_password_is_null_or_whitespace_SetPassword_method_should_throw_DomainException(string lastName)
+        {
+            Assert.Throws<DomainException>(() =>
+            {
+                // Act & Assert
+                _user.SetPassword(lastName, _passwordHasher);
+            });
+        }
+    }
+}
