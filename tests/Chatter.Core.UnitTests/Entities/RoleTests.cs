68008cb725672cef1513e49165b07c091c10d0c1
Krzysztof Strugała <kr.strugala@gmail.com>
2021-09-13 21:36:53 +0200

    Add Role tests

---
+++ tests/Chatter.Core.UnitTests/Entities/RoleTests.cs
@@ -0,0 +1,53 @@
+﻿using Chatter.Core.Entities;
+using Xunit;
+
+namespace Chatter.Core.UnitTests.Entities
+{
+    public class RoleTests
+    {
+        [Fact]
+        public void IsValid_ShouldReturnTrue_WhenRoleIsEqualToUser()
+        {
+            // Arrange
+            var role = Role.User;
+            // Act
+            var result = Role.IsValid(role);
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsValid_ShouldReturnTrue_WhenRoleIsEqualToAdmin()
+        {
+            // Arrange
+            var role = Role.Admin;
+            // Act
+            var result = Role.IsValid(role);
+            // Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(" ")]
+        public void IsValid_ShouldReturnFalse_WhenRoleIsNullOrWhitespace(string role)
+        {
+            // Arrange & Act
+            var result = Role.IsValid(role);
+            // Assert
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData("manager")]
+        [InlineData("Moderator")]
+        [InlineData("Administrator")]
+        public void IsValid_ShouldReturnFalse_WhenRoleIsInvalid(string role)
+        {
+            // Arrange & Act
+            var result = Role.IsValid(role);
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
